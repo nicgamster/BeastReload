84ed0f9a7b667000d17fcb79a8a4ac23b19df2ab
Zelimkhan <nic-gamster@yandex.ru>
2025-10-01 20:42:02 +0300

    start

---
+++ BeastReload/BeastManager.cs
@@ -0,0 +1,92 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using Progress;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using UnityEngine;
+
+namespace BeastReload
+{
+    [BepInPlugin("BeastReload", "BeastReload", "1.0.0")]
+    public class BeastManager : BaseUnityPlugin
+    {
+        public static ConfigEntry<bool> SkipStartIntro;
+        public static ConfigEntry<bool> SkipIntroCutscene;
+        public static ConfigEntry<bool> AutoStartGame;
+        public static ConfigEntry<bool> InfiniteAmmo;
+
+
+        private void Awake()
+        {
+            SkipStartIntro = Config.Bind(
+                "Settings",
+                "Skip Start Game Cutscene",
+                true,
+                "Skip the initial intro animation"
+            );
+
+            SkipIntroCutscene = Config.Bind(
+                "Settings",
+                "Skip Intro Cutscene",
+                true,
+                "Skip the initial intro animation"
+            );
+
+            AutoStartGame = Config.Bind(
+                "Settings",
+                "Auto Start Game",
+                true,
+                "Automatically start game after skipping intro"
+            );
+
+            InfiniteAmmo = Config.Bind(
+                "Settings",
+                "Infinite Ammo",
+                true,
+                "Bullet Rain"
+            );
+
+            var harmony = new Harmony("BeastReload");
+            harmony.PatchAll();
+            Logger.LogInfo("BeastReload mod loaded!");
+        }
+    }
+
+    [HarmonyPatch(typeof(ProgressManager), "ShouldDisplayGameIntroOnStart")]
+    public static class BypassIntroCheck_Patch
+    {
+        private static void Postfix(ref bool __result)
+        {
+            if (BeastManager.SkipIntroCutscene.Value)
+            {
+                __result = false; // Говорим что интро уже показывалось
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(LevelController), "Initialize")]
+    public static class SkipStartScreenIntro_Patch
+    {
+        private static void Prefix(Camera startingCamera, ref bool newLevelAttempt)
+        {
+            if (BeastManager.SkipIntroCutscene.Value)
+            {
+                newLevelAttempt = false;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(DebugManager), "GetInfiniteAmmo")]
+    public static class InfiniteAmmo_Patch
+    {
+        private static void Postfix(ref bool __result)
+        {
+            if (BeastManager.InfiniteAmmo.Value)
+            {
+                __result = true;
+            }
+        }
+    }
+}
